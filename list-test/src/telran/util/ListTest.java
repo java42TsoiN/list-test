267f1463741cab17172e188f5234edf9fad213f6
NTsoi <czoj.n95@mail.ru>
2021-11-07 21:09:32 +0200

    CW #10

--- list-test/src/telran/util/ListTest.java
+++ list-test/src/telran/util/ListTest.java
@@ -34,8 +34,8 @@ String initialStrings[] = {"name1", "name2"};
 
 		//List<Integer> res = new ArrayList<>(1);
 		List<Integer> res = new LinkedList<>();
-		for (int i = 0; i < initialNumbers.length; i++) {
-			res.add(initialNumbers[i]);
+		for (int num:initialNumbers) {
+			res.add(num);
 		}
 		return res;
 	}
@@ -138,9 +138,9 @@ String initialStrings[] = {"name1", "name2"};
 	private <T> T[] getArrayFromList(List<T> list) {
 		int size = list.size();
 		T[] res = (T[]) new Object[size];
-		
-		for (int i = 0; i < size; i++) {
-			res[i] = list.get(i);
+		int resInd=0;
+		for(T obj:list) {
+			res[resInd++]=obj;
 		}
 		return res;
 	}
@@ -265,7 +265,10 @@ String initialStrings[] = {"name1", "name2"};
 		
 	}
 	
-	
+	@Test
+	void removeByIteratorTest() {
+		//TODO test for checking method remove of ArrayListIterator
+	}
 	
 	
 	
